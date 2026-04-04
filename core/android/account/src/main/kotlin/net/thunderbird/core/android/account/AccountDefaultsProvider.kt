df762b0ba93a02dd1a352fbe2c166436679d1341
Josh Law <joshlaw48@gmail.com>
2026-04-04 20:26:15 +0000

    account: default to plain text and enable bottom-posting for replies
    Change-Id: I3467861263904ca7748aa51ef70b2f67afc1941d
    Signed-off-by: Josh Law <joshlaw48@gmail.com>

--- core/android/account/src/main/kotlin/net/thunderbird/core/android/account/AccountDefaultsProvider.kt
+++ core/android/account/src/main/kotlin/net/thunderbird/core/android/account/AccountDefaultsProvider.kt
@@ -21,8 +21,7 @@ interface AccountDefaultsProvider {
         const val DEFAULT_MAXIMUM_AUTO_DOWNLOAD_MESSAGE_SIZE = 131072
 
         @JvmStatic
-        val DEFAULT_MESSAGE_FORMAT = MessageFormat.HTML
-
+        val DEFAULT_MESSAGE_FORMAT = MessageFormat.TEXT
         const val DEFAULT_MESSAGE_FORMAT_AUTO = false
         const val DEFAULT_MESSAGE_READ_RECEIPT = false
         const val DEFAULT_QUOTED_TEXT_SHOWN = true
@@ -32,7 +31,7 @@ interface AccountDefaultsProvider {
         val DEFAULT_QUOTE_STYLE = QuoteStyle.PREFIX
 
         const val DEFAULT_REMOTE_SEARCH_NUM_RESULTS = 25
-        const val DEFAULT_REPLY_AFTER_QUOTE = false
+        const val DEFAULT_REPLY_AFTER_QUOTE = true
         const val DEFAULT_RINGTONE_URI = "content://settings/system/notification_sound"
         const val DEFAULT_SORT_ASCENDING = false
 
