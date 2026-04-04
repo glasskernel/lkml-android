193c6a0521c4b71cd062fb971ea4eeb8b23e92e0
Josh Law <joshlaw48@gmail.com>
2026-04-04 20:26:21 +0000

    privacy: strip User-Agent from outgoing mail headers
    Change-Id: I5f2bd4984f1d57df19ece3c9b09e5e4cff3f77c5
    Signed-off-by: Josh Law <joshlaw48@gmail.com>

--- core/preference/api/src/commonMain/kotlin/net/thunderbird/core/preference/privacy/PrivacySettings.kt
+++ core/preference/api/src/commonMain/kotlin/net/thunderbird/core/preference/privacy/PrivacySettings.kt
@@ -1,7 +1,7 @@
 package net.thunderbird.core.preference.privacy
 
 const val PRIVACY_SETTINGS_DEFAULT_HIDE_TIME_ZONE = false
-const val PRIVACY_SETTINGS_DEFAULT_HIDE_USER_AGENT = false
+const val PRIVACY_SETTINGS_DEFAULT_HIDE_USER_AGENT = true
 
 data class PrivacySettings(
     val isHideTimeZone: Boolean = PRIVACY_SETTINGS_DEFAULT_HIDE_TIME_ZONE,
