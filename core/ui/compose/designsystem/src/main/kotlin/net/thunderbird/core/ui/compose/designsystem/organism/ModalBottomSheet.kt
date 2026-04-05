045a618f873a205ac6dcc3d9caa74548819dd57f
Josh Law <joshlaw48@gmail.com>
2026-04-05 21:36:10 +0000

    feat: apply liquid glass effect to bottom sheets
    Use Haze to render a frosted glass background on ModalBottomSheet instead of a solid color.
    
    Change-Id: Ic2291704518696ee544c329715542a22bda673af
    Signed-off-by: Josh Law <joshlaw48@gmail.com>

--- core/ui/compose/designsystem/src/main/kotlin/net/thunderbird/core/ui/compose/designsystem/organism/ModalBottomSheet.kt
+++ core/ui/compose/designsystem/src/main/kotlin/net/thunderbird/core/ui/compose/designsystem/organism/ModalBottomSheet.kt
@@ -5,6 +5,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import app.k9mail.core.ui.compose.designsystem.template.LocalHazeState
+import dev.chrisbanes.haze.hazeChild
 import androidx.compose.material3.ModalBottomSheet as MaterialModalBottomSheet
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,7 +22,8 @@ fun ModalBottomSheet(
     MaterialModalBottomSheet(
         onDismissRequest = onDismissRequest,
         sheetState = sheetState,
-        modifier = modifier,
+        modifier = modifier.hazeChild(LocalHazeState.current),
+        containerColor = Color.Transparent,
         content = content,
     )
 }
